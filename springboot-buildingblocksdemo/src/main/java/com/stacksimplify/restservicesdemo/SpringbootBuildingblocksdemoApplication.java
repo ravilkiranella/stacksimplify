98cdde449d4d435f2cdd5e2f880c06f4b8e3dc42
pc <pc@DESKTOP-MN3VH4A>
2019-12-22 20:24:55 +0530

    first commit

---
+++ springboot-buildingblocksdemo/src/main/java/com/stacksimplify/restservicesdemo/SpringbootBuildingblocksdemoApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksdemoApplication.class, args);
+	}
+
+}
---
+++ springboot-buildingblocksdemo/src/test/java/com/stacksimplify/restservicesdemo/SpringbootBuildingblocksdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
